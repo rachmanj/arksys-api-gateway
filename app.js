daf81933f98701445d850f37a11a4e95b3d0113c
rachmanj <rachmanj@gmail.com>
2022-07-18 14:24:40 +0800

    buc api

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
-require("dotenv").config();
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
-const payreqsRouter = require("./routes/payreqs");
-const refreshTokensRouter = require("./routes/refreshTokens");
-const verifyToken = require("./middlewares/verifyToken");
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const payreqsRouter = require('./routes/payreqs');
+const refreshTokensRouter = require('./routes/refreshTokens');
+const verifyToken = require('./middlewares/verifyToken');
 
 const app = express();
 
-app.use(logger("dev"));
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: false, limit: "50mb" }));
+app.use(logger('dev'));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/payreqs", payreqsRouter);
-app.use("/refresh-tokens", refreshTokensRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/payreqs', payreqsRouter);
+// app.get('/payreqs', (req, res) => {
+//   res.send('payreqs mantap');
+// });
+app.use('/refresh-tokens', refreshTokensRouter);
 
 module.exports = app;
--- routes/handler/payreqs/getBucs.js
+++ routes/handler/payreqs/getBucs.js
@@ -1,19 +1,19 @@
-const apiAdapter = require("../../apiAdapter");
-const { URL_SERVICE_BUCS } = process.env;
+const apiAdapter = require('../../apiAdapter');
+const { URL_SERVICE_PAYREQ } = process.env;
 
-const api = apiAdapter(URL_SERVICE_BUCS);
+const api = apiAdapter(URL_SERVICE_PAYREQ);
 
 module.exports = async (req, res) => {
   try {
-    const bucs = await api.get("/bucs");
+    const bucs = await api.get('/bucs');
     return res.json(bucs.data);
   } catch (error) {
-    if (error.code === "ECONNREFUSED") {
+    if (error.code === 'ECONNREFUSED') {
       return res
         .status(500)
-        .json({ status: "error", message: "service unavailable" });
+        .json({ status: 'error', message: 'service unavailable' });
     }
     // const { data } = error.response;
-    return res.status(404).json({ message: "service unavailable" });
+    return res.status(404).json({ message: 'service unavailable' });
   }
 };
