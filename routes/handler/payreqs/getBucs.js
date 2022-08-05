d732d5329eb9a506bc54c8ddc44de6b4fc92fbaf
rachmanj <rachmanj@gmail.com>
2022-08-05 09:20:27 +0800

    bucs controller

--- routes/handler/payreqs/getBucs.js
+++ routes/handler/payreqs/getBucs.js
@@ -1,13 +1,36 @@
 const apiAdapter = require('../../apiAdapter');
-const { URL_SERVICE_PAYREQ } = process.env;
+const { URL_SERVICE_PAYREQ, HOSTNAME } = process.env;
 
 const api = apiAdapter(URL_SERVICE_PAYREQ);
 
 module.exports = async (req, res) => {
   try {
-    const bucs = await api.get('/bucs');
+    const bucs = await api.get('/bucs', {
+      params: {
+        ...req.query,
+      },
+    });
 
-    return res.json(bucs.data);
+    const bucsData = bucs.data;
+    const firstPage = bucsData.data.first_page_url.split('?').pop();
+    const lastPage = bucsData.data.last_page_url.split('?').pop();
+
+    bucsData.data.first_page_url = `${HOSTNAME}/payreqs/bucs?${firstPage}`;
+    bucsData.data.last_page_url = `${HOSTNAME}/payreqs/bucs?${lastPage}`;
+
+    if (bucsData.data.next_page_url) {
+      const nextPage = bucsData.data.next_page_url.split('?').pop();
+      bucsData.data.next_page_url = `${HOSTNAME}/payreqs/bucs?${nextPage}`;
+    }
+
+    if (bucsData.data.prev_page_url) {
+      const prevPage = bucsData.data.prev_page_url.split('?').pop();
+      bucsData.data.prev_page_url = `${HOSTNAME}/payreqs/bucs?${prevPage}`;
+    }
+
+    bucsData.data.path = `${HOSTNAME}/payreqs/bucs`;
+
+    return res.json(bucsData);
   } catch (error) {
     if (error.code === 'ECONNREFUSED') {
       return res
